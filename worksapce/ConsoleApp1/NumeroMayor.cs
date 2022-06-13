3437ad1e0be89146fda6ecb55cd39b5d76e60b85
jc-gi <juancarlos.gonzalez@upslp.edu.mx>
2022-06-13 10:51:32 -0500

    Add files via upload

---
+++ worksapce/ConsoleApp1/NumeroMayor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "El mayor de dos numeros";
+            int x;
+            int y;
+            Console.WriteLine("Digita el primer numero a comparar");
+            Console.WriteLine("Entre el 1 y el 100");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digita el segundo numero a comparar");
+            Console.WriteLine("Entre el 1 y el 100");
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine("\nEl numero {0} es mayor que {1}", x, y);
+            }
+            else
+            {
+                Console.WriteLine("\nEl numero {0} es mayor que {1}", y, x);
+            }
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\n----->Fin del programa");
+            Console.ReadKey();
+        }     
+    }
+}
---
+++ worksapce/ConsoleApp2/opcion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            int numero1=2;
+            int numero2=3;
+            int suma=0;
+            Console.Title = "SWITCH CASE";
+            Console.WriteLine("\n\tOPERACIONES MATEMATICAS");
+            Console.WriteLine("\n\t1. SUMAR");
+            Console.WriteLine("\n\t2. RESTAR");
+            Console.WriteLine("\n\t3. MULTIPLICAR");
+            Console.WriteLine("\n\t4. DIVIDIR");
+            Console.WriteLine("\n\t5. SALIR DEL PROGRAMA");
+            Console.WriteLine("\n\n\t Digite su opcion: ");
+            opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    suma = numero1 + numero2;
+                    Console.WriteLine("\t El resultado de la suma es: " + suma);
+                    break;
+                case 2:
+                    Console.WriteLine("\t El resultado de la resta es: ");
+                    break;
+                case 3:
+                    Console.WriteLine("\t El resultado de la multiplicacion es: ");
+                    break;
+                case 4:
+                    Console.WriteLine("\t El resultado de la division es: ");
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("\t Opcion no definida..intente de nuevo");
+                    break; 
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
